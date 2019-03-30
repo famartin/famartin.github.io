6b533330897861d2effa6fde9846587bd2f237e6
Faustino Martinez <faustmtz@protonmail.com>
2019-03-29 21:05:43 -0500

    Updates

---
+++ precache-manifest.dd99f144a512dafd4148778133da2e4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd344dfa290a4a645146",
+    "url": "/static/css/main.094a29ca.chunk.css"
+  },
+  {
+    "revision": "bd344dfa290a4a645146",
+    "url": "/static/js/main.958f7d56.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "157dca95162c0c0c5532f513928743ce",
+    "url": "/index.html"
+  }
+];
